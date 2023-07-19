3b4fe7ac0220a1ba4f0bc07bcb40eb1824a2f195
firecraft-111 <kksumit13@gmail.com>
2023-07-19 11:29:47 +0530

    first_commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components"
+import Hero from "./Component/Hero"
+import Who from "./Component/Who"  
+import Works from "./Component/Works"
+import Contact from "./Component/Contact"
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url('./img/bg.jpeg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  &::-webkit-scrollbar{
+     display: none;
+  }
+`;
+
+
+function App() {
+
+  return (
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/Component/Who.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center ;
+`;
+
+const Who= () => {
+    return (
+        <Section>Hero</Section>
+    )
+}
+
+export default Who
